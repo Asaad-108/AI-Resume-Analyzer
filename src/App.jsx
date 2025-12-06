e6a03e4d08df767709019007dee976e3b6a5f6cf
Asaad-108 <asadasghar836@gmail.com>
2025-12-06 21:39:29 +0500

    Making design responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,30 @@ function App() {
   return (
     <>
     <h2>AI RESUME ANALYZER</h2>
-    <div className='container'>
+    <div className="maincontainer">
+      <div className='container'>
       <h4>Upload your resume</h4>
       <p>Get instant feedback on your resume with AI-powered analysis</p>
-      <div className="upload">
+      <form action="" method="post">
+        <input type="file" className='file' id='file'/>
+        <label htmlFor="file">
+          <div className="uploaddone">
         <div className='imagediv'>
-          <img src="src/assets/upload.svg" alt="uploadicon" className='icon' />
+          <img src="src/assets/upload.svg" alt="uploadicon" className='icondone' />
         </div>
         <p className='text'>Click to upload your resume</p>
         <p className='text'>or drag and drop</p>
         <p className='text'>PDF or DOCX (MAX 5MB)</p>
       </div>
+        </label>
+      </form>
       <div className='btndiv'>
-        <button className='btn'>Analyze Resume</button>
+        <button className='btndone'>Analyze Resume</button>
       </div>
     </div>
-    <div className="bottom">
+    </div>
+    <div className="mainbotton">
+          <div className="bottom">
       <div className="box box1">
         <div>
           <img src="src/assets/target.png" alt="target" />
@@ -42,6 +50,7 @@ function App() {
         <p className='p1'>Detailed Report</p>
         <p className='p2'>Download PDF report</p>
       </div>
+    </div>
     </div>
     </>
   )
